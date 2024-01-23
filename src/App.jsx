ec680257e2aa4adb89dd28140c60beba6141a2f9
SHoang6702 <shoang6702@gmail.com>
2024-01-23 05:23:27 -0600

    wip importing

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Spline from "@splinetool/react-spline";
+import { useState } from "react";
+import { IoMenu, IoLogoGithub } from "react-icons/io5";
+import Photo from "./images/knight.jpg";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { Experience } from "./components/UI/TimelineData.jsx";
+import { Projects } from "./components/UI/ProjectsData.jsx";
+import { SocialMedia } from "./components/UI/ContactData.jsx";
+import { AnimatePresence, motion } from "framer-motion";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
