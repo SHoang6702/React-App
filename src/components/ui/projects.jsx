308244dfdef9e9b17c4bc73f7b8edb3418e94d8f
SHoang6702 <shoang6702@gmail.com>
2024-01-23 20:16:29 -0600

    changes to links

--- src/components/ui/projects.jsx
+++ src/components/ui/projects.jsx
@@ -9,18 +9,18 @@ export const Projects = [
     id: 1,
     name: "REGEX Email Tutorial",
     image: REGEXTutorial,
-    github: "https://github.com/SHoang6702/03b8c91217826d0f8d5ae5dfa284e642",
+    github: "https://gist.github.com/SHoang6702/03b8c91217826d0f8d5ae5dfa284e642",
   },
   {
     id: 2,
     name: "PWA Text Editor",
     image: textEditor,
-    github: "https://github.com/SHoang6702/SHoang_Text_Editor",
+    github: "https://just-another-text-editor-dmjs.onrender.com/",
   },
   {
     id: 3,
     name: "Workday Scheduler",
     image: workdayScheduler,
-    github: "https://https://shoang6702.github.io/WorkDayScheduler/",
+    github: "https://shoang6702.github.io/WorkDayScheduler/",
   },
 ];
